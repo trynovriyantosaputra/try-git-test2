124864d2f911600a00c03b78ccea001aa3cdbc4c
trynovriyantosaputra <trynovriyantosaputra@gmail.com>
2022-10-21 11:40:39 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -11,6 +11,10 @@
 <body>
     <h1>Hello Earth!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque expedita maxime et error deleniti nisi animi explicabo atque, temporibus, excepturi harum? Corrupti dolore sint, ducimus placeat nisi vitae unde molestias!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
